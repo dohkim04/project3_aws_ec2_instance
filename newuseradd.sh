bd3c17d6be9d0861e85f48d9c54e789616770a1d
Do Hyung Kim <doh.kim04@gmail.com>
2023-02-11 03:26:11 +0000

    add my second bash script: adding a new user to a linux server

---
+++ newuseradd.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# a simple script to accept a single user.
+echo "Please enter new user name to add to our system:"
+read name
+echo "Please wait while we are processing your request"
+sleep 1
+echo "Check whether the entered string follows the Linux user name rules..."
+sleep 2
+if [ -z $name ]
+then
+  echo -e "\n$name is empty. please try again"
+  exit 1
+else
+  if ! [[ "$name" =~ ^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$ ]]
+  then
+    echo -e "\nError: $name does not follow the correct name rule. Try again"
+    exit 1
+  else
+    echo -e "\n$name does follow the correct name rule. Will create username"
+    sudo useradd $name
+    exit 0
+  fi
+fi
+# "==================================================================="
+# "This script was created by Do Hyung Kim on Feb. 10, 2023."
+# "Please acknowledge this is the original work when you make changes."
+# "Thank you so much!"
+# "==================================================================="
