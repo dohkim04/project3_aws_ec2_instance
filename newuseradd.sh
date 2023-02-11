d098cb514d654200e324ef853970fc3cadf06a59
Do Hyung Kim <doh.kim04@gmail.com>
2023-02-11 08:18:13 +0000

    edited newuseradd script file

--- newuseradd.sh
+++ newuseradd.sh
@@ -11,13 +11,14 @@ then
   echo -e "\n$name is empty. please try again"
   exit 1
 else
-  if ! [[ "$name" =~ ^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$ ]]
+#  if ! [[ "$name" =~ ^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$ ]]
+  if ! [[ "$name" =~ ^[a-z_][a-z0-9_-]{0,31}$ ]] #EC2 Linux cannot accept postfix $ sign as user name
   then
     echo -e "\nError: $name does not follow the correct name rule. Try again"
     exit 1
   else
     echo -e "\n$name does follow the correct name rule. Will create username"
-    sudo useradd $name
+    sudo useradd -d $name
     exit 0
   fi
 fi
